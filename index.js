ee7ef6b2f8a189c6fe51b4a4eea2c7af750a16d2
Alexander P <panshin.alexander@yahoo.com>
2021-08-27 00:24:02 +0300

    Вывод файла на страницу

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var fs = require("fs");
+var http = require("http");
 
-var myReader = fs.createReadStream(__dirname + "/BigTextFile.txt")
-var myWriter = fs.createWriteStream(__dirname + "/NewBigTextFile.txt")
 
-myReader.pipe(myWriter)
+
+
+var server = http.createServer(function(request, response) {
+  console.log("URL " + request.url);
+  response.writeHead(200, {"Content-Type" : "text/plain; charset=utf-8"});
+
+  var myReader = fs.createReadStream(__dirname + "/BigTextFile.txt")
+  myReader.pipe(response)
+});
+
+server.listen(3333, "127.0.0.1");
+console.log("LISTEN PORT 3333");
