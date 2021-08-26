bd370bd8ff889b68fbf635f14875253a19f901ed
Alexander P <panshin.alexander@yahoo.com>
2021-08-27 00:30:33 +0300

    Вывод JSON на страницу

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var fs = require("fs");
 var http = require("http");
-
-
-
 
 var server = http.createServer(function(request, response) {
   console.log("URL " + request.url);
-  response.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
-
-  var myReader = fs.createReadStream(__dirname + "/index.html")
-  myReader.pipe(response)
+  response.writeHead(200, {"Content-Type" : "application/json; charset=utf-8"});
+  var obj = {
+    type: "type1",
+    text: "text example"
+  };
+  response.end(JSON.stringify(obj));
 });
 
 server.listen(3333, "127.0.0.1");
