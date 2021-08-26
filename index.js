44a2ca3d1e7093ce553498e191bd7bdedb68b5bc
Alexander P <panshin.alexander@yahoo.com>
2021-08-26 21:44:59 +0300

    Чтение из файла

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-var events = require("events")
-var util = require("util")
+var fs = require("fs")
 
-var cars = function(model) {
-  this.model = model;
-}
-
-util.inherits(cars, events.EventEmitter);
-
-var audi = new cars("audi");
-var lada = new cars("lada");
-var uaz = new cars("uaz");
-
-var carArray = [audi, lada, uaz];
-carArray.forEach(function(car) {
-  car.on("speed", function(text) {
-    console.log(car.model + " speed is " + text);
-  });
-});
-
-lada.emit("speed", "120 kmh")
-uaz.emit("speed", "65 kmh")
+console.log(fs.readFileSync("file.txt", "utf8"));
