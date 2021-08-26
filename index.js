5c5005a37238119e33d74c6d33836b3b5367f93b
Alexander P <panshin.alexander@yahoo.com>
2021-08-27 00:03:31 +0300

    Простейший сервер

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var fs = require("fs")
+var http = require("http");
 
-fs.mkdirSync("new_dir");
-fs.rmdirSync("new_dir");
+var server = http.createServer(function(request, response) {
+  console.log("URL " + request.url);
+  response.writeHead(200, {"Content-Type" : "text/plain; charset=utf-8"});
+  response.end("HELLO WORLD.")
+});
+
+server.listen(3333, "127.0.0.1");
+console.log("LISTEN PORT 3333");
