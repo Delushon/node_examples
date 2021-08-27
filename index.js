7fe63a9045d9d71fefdbd95d6efe4721ef72d51e
Alexander P <panshin.alexander@yahoo.com>
2021-08-28 00:03:30 +0300

    данные от клиента

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 let express = require("express");
+let bodyParser = require('body-parser')
 
 let app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 
 app.use("/public", express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", function (request, response) {
-  response.render("index");
+    response.render("index");
 });
 
 app.get("/:id", function (request, response) {
-  response.send("Id = " + request.params.id);
+    response.send("Id = " + request.params.id);
 });
 
 app.get("/news/:id", function (request, response) {
-  response.render("news", {newsId: request.params.id});
+    response.render("news", {newsId: request.params.id});
 });
+
+app.post('/news/:id', urlencodedParser, function (req, res) {
+    console.log(req.body)
+})
 
 app.listen(3333)
