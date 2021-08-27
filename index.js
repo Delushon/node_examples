a24969ed299a22df9a367b2691ca0966e190661d
Alexander P <panshin.alexander@yahoo.com>
2021-08-27 22:57:16 +0300

    стили

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let express = require("express");
 
 let app = express();
 
+app.use("/public", express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get("/", function (request, response) {
