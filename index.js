fc80b778a75a38d80cfabf3fbc9e413e5e36cf9a
Alexander P <panshin.alexander@yahoo.com>
2021-08-27 21:01:45 +0300

    Добавление express

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ var http = require("http");
 var server = http.createServer(function(request, response) {
   console.log("URL " + request.url);
   response.writeHead(200, {"Content-Type" : "application/json; charset=utf-8"});
-  var obj = {
-    type: "type1",
-    text: "text example"
-  };
-  response.end(JSON.stringify(obj));
+
+  response.end(request.url);
 });
 
 server.listen(3333, "127.0.0.1");
